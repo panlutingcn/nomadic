50ce8e64339fe6440251e7a3ac945034934d3a2c
panlutingcn <panlutingcn@gmail.com>
2026-04-30 23:22:56 +0800

    fix: change icon contentType to png to resolve Cloudflare edge runtime error

--- app/icon.tsx
+++ app/icon.tsx
@@ -7,24 +7,35 @@ export const size = {
   height: 32,
 }
 
-export const contentType = 'image/svg+xml'
+export const contentType = 'image/png'
 
 export default function Icon() {
   return new ImageResponse(
     (
-      <svg
-        width="32"
-        height="32"
-        viewBox="0 0 32 32"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
+      <div
+        style={{
+          width: 32,
+          height: 32,
+          borderRadius: 6,
+          background: '#1d9e75',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
       >
-        <rect width="32" height="32" rx="6" fill="#1d9e75" />
-        <path
-          d="M16 8L20 14H12L16 8Z M16 24L12 18H20L16 24Z"
-          fill="white"
-        />
-      </svg>
+        <svg
+          width="18"
+          height="18"
+          viewBox="0 0 32 32"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M16 8L20 14H12L16 8Z M16 24L12 18H20L16 24Z"
+            fill="white"
+          />
+        </svg>
+      </div>
     ),
     {
       ...size,
